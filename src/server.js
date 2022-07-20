5562542dbe06c2c113b12f4b4040295d97894315
Xolile Xaba <xolile@medyear.com>
2022-07-20 17:13:03 +0200

    changes on server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/joie2'));
+app.use(express.static(__dirname + '/dist/joie2/index.html'));
 
 app.get('/*', function(req,res) {
     
