80fb443e845a77c48d98cb84ad9edbbf00109696
Xolile Xaba <xolile@medyear.com>
2022-07-20 18:35:07 +0200

    Directory

--- src/server.js
+++ src/server.js
@@ -5,12 +5,19 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/joie2/index.html'));
+app.use(express.static(__dirname + '/dist/joie2'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/joie2/index.html'));
-});
+
+    if(req.url === '/') {
+      res.sendFile(path.join(__dirname,'/dist/joie2/index.html'));
+    }
+    else {
+      res.sendFile(path.join(__dirname, '/dist/joie2' + req.url))
+    }
+  
+  });
+  
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
