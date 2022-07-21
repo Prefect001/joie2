84256b063a9dac7d74cdc6dd5c89b98b96666607
Xolile Xaba <xolile@medyear.com>
2022-07-21 03:57:53 +0200

    dist changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let app = express();
 app.use(express.static(__dirname + '/dist/joie2'));
 
 app.get('/*', (req, resp)=>{
-    resp.sendFile(__dirname+'/dist/joie2/index.html');
+    resp.sendFile(__dirname+'/dist/Joie2/index.html');
 
 });
 
