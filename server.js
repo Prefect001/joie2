3e3327c0ae0916d732f38b910489c7c098ac7ce3
Xolile Xaba <xolile@medyear.com>
2022-07-21 04:03:58 +0200

    server changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname + '/dist/joie2'));
+app.use(express.static(__dirname + '/dist/Joie2'));
 
 app.get('/*', (req, resp)=>{
     resp.sendFile(__dirname+'/dist/Joie2/index.html');
